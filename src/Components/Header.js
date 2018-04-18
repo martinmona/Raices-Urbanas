0a740b576a5b4a8d5142179bc7621bde2e26daf8
martinmona <martinmona@users.noreply.github.com>
2018-04-17 23:38:40 -0300

    cabecera

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <header className="App-header">
+        <header className="Cabecera">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
